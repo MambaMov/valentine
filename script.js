4e101c07c640a367a35e4bbd974cb2634d7eeb67
oiksdhihfsdbljdfvkujbfduhjkuhjkgfdfdg <46480911+MambaMov@users.noreply.github.com>
2025-02-05 21:44:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const messages = [
+    "Chiar nu vrei printesa?",
+    "Esti sigura ale?",
+    "Deci asa vrei...",
+    "Ok",
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
